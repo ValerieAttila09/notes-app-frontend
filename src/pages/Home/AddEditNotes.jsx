6bbbb9ae177f21052a96c5efac0b2d6ad6b805e9
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-21 20:59:41 +0700

    committed

---
+++ src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,20 @@
+export default function AddEditNotes() {
+  return (
+    <div className="">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="" className="input-label">Title</label>
+        <input type="text" className="text-2xl text-neutral-900 outline-none" placeholder="Go To Gym at 5" name="" id="" />
+      </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <label htmlFor="" className="input-label">Content</label>
+        <textarea className="text-sm text-neutral-900 outline-none bg-neutral-50 p-2 rounded-md" placeholder="Content" name="" id="" rows={10}></textarea>
+      </div>
+
+      <div className="mt-3">
+        <label htmlFor="" className="input-label">Tags</label>
+      </div>
+
+      <button className="w-full bg-indigo-500 text-white outfit-medium mt-5 p-3" onClick={() => {}}>Add</button>
+    </div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import NoteCard from "../../components/Cards/NoteCard";
 import Navbar from "../../components/Navbar/Navbar";
+import AddEditNotes from "./AddEditNotes";
+import ReactModal from "react-modal";
 
 export default function Home() {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null
+  })
+
   return (
     <div className="">
       <Navbar />
@@ -21,11 +31,34 @@ export default function Home() {
         </div>
       </div>
 
-      <button className="w-14 h-14 flex items-center justify-center rounded-2xl bg-indigo-500 hover:shadow-lg hover:bg-indigo-600 transition-all absolute right-10 bottom-10 " onClick={() => { }}>
+      <button
+        className="w-14 h-14 flex items-center justify-center rounded-2xl bg-indigo-500 hover:shadow-lg hover:bg-indigo-600 transition-all absolute right-10 bottom-10 "
+        onClick={() => {
+          setOpenAddEditModal({
+            isShown: true,
+            type: "add",
+            data: null
+          })
+        }}
+      >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
       </button>
+
+      <ReactModal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => { }}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)"
+          }
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes />
+      </ReactModal>
 
     </div>
   )
